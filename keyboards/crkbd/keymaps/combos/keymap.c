02960b55706b3808f4a0c4fa566ed03d9d41acea
Filip Alberius <filip@alberius.se>
2025-05-22 12:40:24 +0200

    update layout

--- keyboards/crkbd/keymaps/combos/keymap.c
+++ keyboards/crkbd/keymaps/combos/keymap.c
@@ -22,16 +22,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 #include <keymap_swedish.h>
 
-enum custom_keycodes {
-    ALT_TAB = SAFE_RANGE
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3( // QWERTY
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_TAB,     SE_Q,    SE_W,    SE_E,    SE_R,    SE_T,                         SE_Y,    SE_U,    SE_I,    SE_O,   SE_P,   KC_DEL,
+      KC_TAB,     SE_Q,    SE_W,    SE_E,    SE_R,    SE_T,                         SE_Y,    SE_U,    SE_I,    SE_O,    SE_P, SE_ARNG,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    SE_A,    SE_S,    SE_D,    SE_F,    SE_G,                         SE_H,    SE_J,    SE_K,    SE_L, SE_QUOT,  KC_ENT,
+      KC_DEL,     SE_A,    SE_S,    SE_D,    SE_F,    SE_G,                         SE_H,    SE_J,    SE_K,    SE_L, SE_ODIA, SE_ADIA,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    SE_Z,    SE_X,    SE_C,    SE_V,    SE_B,                         SE_N,    SE_M, SE_COMM,  SE_DOT, SE_MINS, KC_LGUI,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -41,33 +37,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT_split_3x6_3( //Numpad
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      ALT_TAB, XXXXXXX,    SE_7,    SE_8,    SE_9, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX,  SE_DLR,    SE_7,    SE_8,    SE_9, SE_CIRC,                        SE_AT, SE_RPRN, SE_RCBR, SE_RBRC, SE_RABK, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX,    SE_4,    SE_5,    SE_6, XXXXXXX,                      XXXXXXX, SE_LPRN, SE_LCBR, SE_LBRC, SE_LABK, XXXXXXX,
+      XXXXXXX, SE_HASH,    SE_4,    SE_5,    SE_6, SE_EQL,                       SE_PIPE, SE_LPRN, SE_LCBR, SE_LBRC, SE_LABK, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+---------+-------+--------|
-      KC_LCTL,    SE_0,    SE_1,    SE_2,    SE_3, XXXXXXX,                      XXXXXXX, SE_RPRN, SE_RCBR, SE_RBRC, SE_RABK, XXXXXXX,
+      XXXXXXX,    SE_0,    SE_1,    SE_2,    SE_3, SE_PLUS,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LSFT,  KC_SPC, _______,      MO(3), KC_BSPC, XXXXXXX
+                                          KC_LSFT,  KC_SPC, _______,      MO(3), KC_BSPC, KC_LALT
                                       //`--------------------------'  `--------------------------'
   ),
 
   [2] = LAYOUT_split_3x6_3( //Symbols
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, SE_PIPE, SE_PERC, SE_AMPR, XXXXXXX,                     XXXXXXX, SE_EXLM, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, SE_PERC, SE_BSLS, SE_EXLM, SE_DQUO, SE_TILD,                     XXXXXXX, KC_UNDO, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, SE_CIRC, SE_PLUS,  SE_EQL, XXXXXXX,                     XXXXXXX, SE_QUES, SE_ARNG, SE_ADIA, SE_ODIA, XXXXXXX,
+      XXXXXXX, SE_AMPR, SE_SLSH, SE_QUES, SE_QUOT, SE_ASTR,                     KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, XXXXXXX, 
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX , SE_SLSH, SE_BSLS, SE_DQUO, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX,  KC_CUT, KC_COPY, KC_PSTE, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LSFT,  KC_SPC,   MO(3),    _______, KC_BSPC, XXXXXXX
+                                          KC_LSFT,  KC_SPC,   MO(3),    _______, KC_BSPC, KC_LALT
                                       //`--------------------------'  `--------------------------'
   ),
 
   [3] = LAYOUT_split_3x6_3( //Arrows
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, XXXXXXX,   XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU, XXXXXXX,                      XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, XXXXXXX,   KC_LEFT,  KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        MO(4), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -90,25 +86,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 // combos
-const uint16_t PROGMEM combo_qwerty_escape[] = {KC_TAB, SE_Q,  SE_W, COMBO_END};
+const uint16_t PROGMEM combo_qwerty_escape[] = {SE_S, SE_D,  SE_F, COMBO_END};
+const uint16_t PROGMEM combo_qwerty_enter[] = {SE_J, SE_K,  SE_L, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
-  COMBO(combo_qwerty_escape,     KC_ESC)
+  COMBO(combo_qwerty_escape,     KC_ESC),
+  COMBO(combo_qwerty_enter,      KC_ENT)
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case ALT_TAB:
-            if (record->event.pressed) {
-                // when keycode ALT_TAB is pressed
-                register_code(KC_LALT);  // press the lalt key
-                register_code(KC_TAB);  // press the tab key
-            } else {
-                // when keycode ALT_TAB is released
-                unregister_code(KC_TAB);  // release the tab key
-                unregister_code(KC_LALT);  // release the lalt key
-            }
-            break;
-    }
-    return true;
-}
